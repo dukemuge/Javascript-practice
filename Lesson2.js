931b726585cd67f3087753a47485c45b3c694b70
dukemuge <dukemuge232@gmail.com>
2021-10-31 05:48:24 -0400

    use strict  and Tested es-lint

--- Lesson2.js
+++ Lesson2.js
@@ -5,3 +5,24 @@
 
  //Using  Filter method
  console.log(languages.filter(word => word.startsWith("J")));
+
+ //strict  equality  check
+
+ const  a =  "1";
+ const b = "1.0";
+ const c = 1;
+
+ console.log(a === b);
+ console.log(c == a);
+
+
+ //Testing  variable scopes
+ //Local and  global scopes  
+
+ const oops =  function(){
+     let haha = 3;
+     console.log(haha);
+ }
+
+
+ console.log(oops);
