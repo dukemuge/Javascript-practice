b54f7b523156c3695afe5bd06931899165116174
dukemuge <dukemuge232@gmail.com>
2021-10-31 03:47:00 -0400

    Add javascript array method and equality check

---
+++ Lesson2.js
@@ -0,0 +1,7 @@
+ const languages = ["Java","Python","Jypter"];
+ const word = "Hello"
+
+ console.log(word.startsWith("H"));
+
+ //Using  Filter method
+ console.log(languages.filter(word => word.startsWith("J")));
